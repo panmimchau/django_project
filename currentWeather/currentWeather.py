7edc8e682479f71f0473544644317bb1f4f68f3f
Michał Gajlewicz <gajlewicz.m@gmail.com>
2021-10-14 06:39:01 +0200

    delete unnecessary comments

--- currentWeather/currentWeather.py
+++ currentWeather/currentWeather.py
@@ -7,20 +7,16 @@ import requests
 import sys
 import pprint
 
-#compute location from command line arguments
+
 if len(sys.argv) < 2:
     print('Usage: get OpenWeather.py city_name, 2-letter_country_code')
     sys.exit()
 location = ''.join(sys.argv[1:])
-#download  the JSON data from OpenWeatherMap.org's API
-#To make the URL we want to access
-#we use the %s placeholder and insert whatever string is stored in location into that spot in the URL string
 url = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s' % (location, APPID)
 response = requests.get(url)
 response.raise_for_status()
 #uncomment to see the raw json txt:
 #print(response.text)
-#load json  data into a Python variable
 weatherData = json.loads(response.text)
 #pprint.pprint(weatherData)
 #print weather descriptions
